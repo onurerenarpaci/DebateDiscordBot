284a8cf6f26d88eaf5922224783744f696f898a6
Onur Eren Arpaci <onurerenarpaci@hotmail.com>
2020-09-06 13:52:07 +0300

    onur_learning update

---
+++ Onur_Learning/discordBot.py
@@ -0,0 +1,65 @@
+# bot.py
+import os
+import asyncio
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('GUILD_NAME')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	guild = discord.utils.get(client.guilds, name=GUILD)
+	print(
+		f'{client.user} has connected to the following guild:\n'
+		f'{guild.name}(id: {guild.id})\n'
+	)
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+	
+	if message.content == 'hello!':
+		await message.channel.send(f'Hello, {message.author.name}!')
+	
+	if message.content == "!countdown":
+		await countdown(message)
+		
+	if message.content == "!check-in":
+		await checkin(message)
+	
+async def countdown(message):
+	minutes = 14
+	seconds = 0
+	message = await message.channel.send(f"`{minutes:02}:{seconds:02}` minutes left.")
+	while not (minutes <= 0 and seconds <= 0):
+		await asyncio.sleep(5)
+		if seconds == 0:
+			minutes = minutes - 1
+			seconds = 55
+			msg = f"`{minutes:02}:{seconds:02}` minutes left."
+			await message.edit(content=msg)
+		else: 
+			seconds = seconds - 5
+			msg = f"`{minutes:02}:{seconds:02}` minutes left."
+			await message.edit(content=msg)
+	await message.edit(content='Time is up!')
+	
+async def checkin(message):
+	message = await message.channel.send("Press the green button to check-in")
+	await message.add_reaction("🟩")
+	await asyncio.sleep(10)
+	message = await message.channel.fetch_message(message.id)
+	green_reaction = str(message.reactions[0].emoji)
+	print(green_reaction)#next(x for x in message.reactions if str(x.emoji) == "🟩")
+	users = await message.reactions[0].users().flatten()#green_reaction.users().flatten()
+	#print(x.name for x in users) 
+	await message.channel.send(" ".join(x.name for x in users))
+	
+
+client.run(TOKEN)
+
