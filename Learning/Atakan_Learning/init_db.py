29665f85024589deec94786dff33cda74a10d833
Atakan Kara <atakankara_48@hotmail.com>
2020-09-18 17:17:16 +0300

    added creating private rooms function to init_db

--- Learning/Atakan_Learning/init_db.py
+++ Learning/Atakan_Learning/init_db.py
@@ -1,6 +1,8 @@
 import mysql.connector
 import os, requests, sys, csv
 from dotenv import load_dotenv
+import discord
+
 
 load_dotenv()
 
@@ -17,8 +19,6 @@ mycursor.execute("CREATE TABLE Participants (name VARCHAR(64), email VARCHAR(64)
 
 #File names
 uniqe_ids_file_name = "unique_ids.txt"
-volunteer_file_name = "İletişim bilgileri.csv"
-
 dirname = os.path.dirname(__file__)
 f = open(os.path.join(dirname, uniqe_ids_file_name))
 unique_ids = f.readlines()
@@ -32,13 +32,17 @@ unique_generator = unique_id_generator()
 teams = requests.get("https://kutab.herokuapp.com/api/v1/tournaments/bp88team/teams",headers=headers).json()
 instit = requests.get("https://kutab.herokuapp.com/api/v1/institutions", headers=headers).json()
 
+#institutions list
 institutions = {}
 for x in instit:
    institutions[x["id"]] = x["code"]
 
+#insterting speakers to database
+teams_list = []
 speakers = []
 speaker = ()
 for team in teams:
+    teams_list.append(team["short_name"])
     for _speaker in team["speakers"]:
         speaker = (_speaker["name"], 
         _speaker["email"],
@@ -61,6 +65,7 @@ mydb.commit()
 print(mycursor.rowcount," speakers was inserted.")
 
 
+#insterting adjudicators to database
 adjudicators = []
 adjudicator = ()
 
@@ -85,24 +90,73 @@ mycursor.executemany(sql, adjudicators)
 mydb.commit()
 print(mycursor.rowcount," juries was inserted.")
 
-# mycursor.execute("Select MAX(id) from Participants")
-# myresult= mycursor.fetchone()
 
-# max_id = myresult[0]
+# #initilazing private rooms
+# TOKEN = os.getenv('DISCORD_TOKEN')
+# GUILD = os.getenv("DISCORD_GUILD")
+# client = discord.Client()
 
-# print(myresult)
-# print(type(myresult[0]))
-# volunteers = []
-# volunteer = ()
-# with open(os.path.join(dirname, volunteer_file_name)) as vol_file:
-#     vol_reader = csv.reader(vol_file, delimiter=",")
-#     for i,row in vol_reader:
-#         volunteer( row[1],
-#         row[2],
-#         "volunteer",
-#         "Koç Üniversitesi",
 
-#         next(unique_generator)[0:-1])
-#         volunteers.append(volunteer)
+# mycursor.execute("CREATE TABLE Private_rooms (name VARCHAR(64), channel_type VARCHAR(64), type VARCHAR(64), id VARCHAR(64))")
 
-# sql = "INSERT INTO Participants (name, email)"
+
+# @client.event
+# async def on_ready():
+#     print(f"{client.user} has connected to Discord!")
+#     guild = discord.utils.get(client.guilds, name=GUILD)
+
+#     instit_category = await guild.create_category("Özel Oda 1")
+#     prep_category = await guild.create_category("Çalışma Odası 1")
+    
+#     text_channel_row = ()
+#     voice_channel_row = ()
+#     instit_room_ids = []
+    
+#     room_count = 1 
+#     i = 0
+    
+#     for _, ins in institutions.items():
+#         text_channel = await instit_category.create_text_channel(ins+"-sohbet")
+#         voice_channel = await instit_category.create_voice_channel(ins+" Ses Kanalı")
+#         text_channel_row = (ins, "text_channel", "institution", text_channel.id)
+#         voice_channel_row = (ins, "voice_channel", "institution", voice_channel.id)
+#         instit_room_ids.append(text_channel_row)
+#         instit_room_ids.append(voice_channel_row)
+#         i = i+2
+#         if i % 50 == 0:
+#             room_count += 1 
+#             instit_category = await guild.create_category(f"Özel Oda {room_count}")
+
+#     sql = "INSERT INTO Private_rooms (name, channel_type, type, id) VALUES (%s, %s, %s, %s)"
+#     mycursor.executemany(sql, instit_room_ids)
+#     print(mycursor.rowcount," institution private channels was inserted.")
+#     mydb.commit()
+    
+
+#     team_room_ids = [] 
+#     room_count = 1
+#     i = 0
+
+#     for team in teams_list:
+#         voice_channel = await prep_category.create_voice_channel(team+" Ses Kanalı")
+#         voice_channel_row = (team, "voice_channel", "team", voice_channel.id)
+#         team_room_ids.append(voice_channel_row)
+#         i = i+1
+#         if i % 50 == 0:
+#             room_count += 1
+#             prep_category = await guild.create_category(f"Çalışma Odası {room_count}")
+
+#     sql = "INSERT INTO Private_rooms (name, channel_type, type, id) VALUES (%s, %s, %s, %s)"
+#     mycursor.executemany(sql, team_room_ids)
+#     print(mycursor.rowcount," team private channels was inserted.")
+#     mydb.commit()
+
+        
+        
+        
+
+    
+
+# client.run(TOKEN)
+
+
