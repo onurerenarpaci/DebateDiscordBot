5ff5b3eeb15f9c078745696b896a5f7187cffa4b
Onur Eren Arpaci <onurerenarpaci@hotmail.com>
2020-09-18 21:54:25 +0300

    beingcut done

--- Learning/Onur_Learning/checkin_deneme/table_init.py
+++ Learning/Onur_Learning/checkin_deneme/table_init.py
@@ -14,11 +14,12 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()    
 
-mycursor.execute("CREATE TABLE Participants (name VARCHAR(64), email VARCHAR(64), role VARCHAR(64) , team VARCHAR(64), team_id VARCHAR(4), institution VARCHAR(64), id INT, url_key VARCHAR(64), checkin BOOLEAN, cut_status BOOLEAN, discord_id VARCHAR(64), unique_id VARCHAR(8)) DEFAULT CHARSET=utf8mb4")
+mycursor.execute("CREATE TABLE Participants (name VARCHAR(64), email VARCHAR(64), role VARCHAR(64) , team VARCHAR(64), team_id VARCHAR(4), institution VARCHAR(64), id INT, url_key VARCHAR(64), checkin BOOLEAN, cut_status BOOLEAN, discord_id VARCHAR(64), unique_id VARCHAR(6)) DEFAULT CHARSET=utf8mb4")
 
-
+#File names
+uniqe_ids_file_name = "unique_ids.txt"
 dirname = os.path.dirname(__file__)
-f = open(os.path.join(dirname, 'unique_ids.txt'))
+f = open(os.path.join(dirname, uniqe_ids_file_name))
 unique_ids = f.readlines()
 f.close()
 
@@ -30,13 +31,17 @@ unique_generator = unique_id_generator()
 teams = requests.get("https://kutab.herokuapp.com/api/v1/tournaments/bp88team/teams",headers=headers).json()
 instit = requests.get("https://kutab.herokuapp.com/api/v1/institutions", headers=headers).json()
 
+#institutions list
 institutions = {}
 for x in instit:
    institutions[x["id"]] = x["code"]
 
+#insterting speakers to database
+teams_list = []
 speakers = []
 speaker = ()
 for team in teams:
+    teams_list.append(team["short_name"])
     for _speaker in team["speakers"]:
         speaker = (_speaker["name"], 
         _speaker["email"],
@@ -52,8 +57,34 @@ for team in teams:
         print(speaker)
         speakers.append(speaker)
 
-sql = "INSERT INTO Participants (name, email, role, team, team_id, institution, id, url_key, cut_status, checkin, unique_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+sql = "INSERT INTO Participants (name, email, role, team, team_id, institution, id, url_key, checkin, cut_status, unique_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
 mycursor.executemany(sql, speakers)
 mydb.commit()
 print(mycursor.rowcount," speakers was inserted.")
+
+
+#insterting adjudicators to database
+adjudicators = []
+adjudicator = ()
+
+adj_list = requests.get("https://kutab.herokuapp.com/api/v1/tournaments/bp88team/adjudicators",headers=headers).json()
+
+for adj in adj_list:
+    adjudicator = (adj["name"],
+    adj["email"],
+    "jury",
+    institutions[int(adj["institution"].split('/')[-1])],
+    adj["id"],
+    adj["url_key"],
+    False,
+    False,
+    next(unique_generator)[0:-1])
+    print(adjudicator)
+    adjudicators.append(adjudicator)
+
+sql = "INSERT INTO Participants (name, email, role, institution, id, url_key, checkin, cut_status, unique_id ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+mycursor.executemany(sql, adjudicators)
+mydb.commit()
+print(mycursor.rowcount," juries was inserted.")
--- checkin/checkin.py
+++ checkin/checkin.py
@@ -1,78 +1,189 @@
-import aiohttp
+# bot.py
+import os
 import asyncio
+import discord
+from discord.ext import commands
+import time
+from dotenv import load_dotenv
+import aiohttp
 import tabbyurl
+import mysql.connector
+import math
 
-channelId = "" #Checkin channel id
+mydb = mysql.connector.connect(
+	host="localhost",
+	user="onur",
+	password="koc2020",
+	database="firstdatabase"
+)
+
+mycursor = mydb.cursor()
+
+checkinId = 739551824612294687 #Checkin channel id
+announcementId = 739551824612294687 # anannouncement channel id
 checkinStatus = False # True = Open, False = Close
 checkinMessage = None
 guild = None # discord.utils.get(client.guilds, name=GUILD)
-checkinDuration = 30 # 30 minutes
+checkinDuration = 1 # 30 minutes
+cutMessageList = []
 
-async def checkin():
-    # adds the checkin messaege and green reaction
-    global checkinMessage
-    global checkinStatus
-    checkinStatus = True
-	message = await guild.get_channel(channelId).send("Check-in yapmak için alttaki 🟩 butonuna basın")
+headers = {"Authorization" : "Token 35792636ef40d2194607066b63e49e3ad3a2076c"}
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('GUILD_NAME')
+
+client = commands.Bot(command_prefix="!")
+
+@client.event
+async def on_ready():
+	global guild
+	guild = discord.utils.get(client.guilds, name=GUILD)
+	print(
+		f'{client.user} has connected to the following guild:\n'
+		f'{guild.name}(id: {guild.id})\n'
+	)
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+		
+	if message.content == "!check-in":
+		await checkin(message)
+	
+	if message.content == "!beingcut":
+		await beingcut()
+	
+async def checkin(message):
+	# adds the checkin messaege and green reaction
+	global checkinMessage
+	global checkinStatus
+	checkinStatus = True
+	message = await guild.get_channel(checkinId).send("Check-in yapmak için alttaki 🟩 butonuna basın")
 	await message.add_reaction("🟩")
 	checkinMessage = message.id
     
     # waits for the indicated duration
-    timeElapsed = 0
-    timestamp = time.time()
-    while timeElapsed < checkinDuration:
-        await asyncio.sleep(10)
-        timeElapsed = int((time.time()-timestamp)/60)
+	timeElapsed = 0
+	timestamp = time.time()
+	while timeElapsed < checkinDuration:
+		await asyncio.sleep(10)
+		timeElapsed = int((time.time()-timestamp)/60)
     
     # deletes the message, closes the checkin, updates the tabbycat
-    await message.delete()
-    checkinStatus = False
-    await guild.get_channel(channelId).send("Check-in süresi doldu.")
-    await checkinUpdate
+	await message.delete()
+	checkinStatus = False
+	await guild.get_channel(checkinId).send("Check-in süresi doldu.")
+	await checkinUpdate()
 
-
+@client.event	
 async def on_reaction_add(reaction, user):
-    if (not checkinStatus) or (not reaction.message.id == checkinMessage) or (not str(reaction.emoji) == "🟩") or user == client.user:
-	    return
+	if (not checkinStatus) or (not reaction.message.id == checkinMessage) or (not str(reaction.emoji) == "🟩") or user == client.user:
+		return
+	
+	print(f'user id={user.id}')
 
-    val = (str(user.id))
-    mycursor.execute("SELECT checkin FROM Participants WHERE discord_id == %s", val)
-    myresult = mycursor.fetchall()
+	val = (str(user.id),)
+	mycursor.execute("SELECT checkin FROM Participants WHERE discord_id = %s", val)
+	myresult = mycursor.fetchall()
 
-    if myresult[0][0]:
-        return
+	if(mycursor.rowcount < 1):
+		print(f'user is not registered: id={str(user.id)} name={str(user.name)}')
+		return
+
+	if myresult[0][0]:
+		return
     
-    val = (True, str(user.id))
-    mycursor.execute("UPDATE Participants SET checkin = %s WHERE discord_id = %s", val)
-    myresult = mycursor.fetchall()
+	val = (True, str(user.id))
+	mydb.commit()
+	mycursor.execute("UPDATE Participants SET checkin = %s WHERE discord_id = %s", val)
+	mydb.commit()
 
-    if user.dm_channel == None: 
+	if user.dm_channel == None: 
 		await user.create_dm()
 	await user.dm_channel.send("You have succesfully checked-in.")
 
     
 async def checkinUpdate():
-    mycursor.execute("SELECT id FROM Participants WHERE checkin = True AND role = speaker")
-    myresult = mycursor.fetchall()
-    session = aiohttp.ClientSession()
+	mycursor.execute("SELECT id FROM Participants WHERE (checkin = True AND role = 'speaker')")
+	myresult = mycursor.fetchall()
+	session = aiohttp.ClientSession()
     
-    for x in myresult:
-        url = f'{tabbyurl.url}/api/v1/tournaments/{tabbyurl.tournament}/speakers/{x[0]}/checkin'
-        async with session.put(url, headers=headers) as resp:
-            print(f'id={x[0]} status={resp.status}')
-            print(await resp.text())
+	for x in myresult:
+		url = f'{tabbyurl.url}/api/v1/tournaments/{tabbyurl.tournament}/speakers/{x[0]}/checkin'
+		async with session.put(url, headers=headers) as resp:
+			print(f'id={x[0]} status={resp.status}')
+			print(await resp.text())
 
-    mycursor.execute("SELECT id FROM Participants WHERE checkin = True AND role = jury")
-    myresult = mycursor.fetchall()
+	mycursor.execute("SELECT id FROM Participants WHERE (checkin = True AND role = 'jury')")
+	myresult = mycursor.fetchall()
 
-    for x in myresult:
-        url = f'{tabbyurl.url}/api/v1/tournaments/{tabbyurl.tournament}/adjudicators/{x[0]}/checkin'
-        async with session.put(url, headers=headers) as resp:
-            print(f'id={x[0]} status={resp.status}')
-            print(await resp.text())
+	for x in myresult:
+		url = f'{tabbyurl.url}/api/v1/tournaments/{tabbyurl.tournament}/adjudicators/{x[0]}/checkin'
+		async with session.put(url, headers=headers) as resp:
+			print(f'id={x[0]} status={resp.status}')
+			print(await resp.text())
 
-    session.close()
+	await session.close()
 
+async def beingcut():
+	global cutMessageList
+	alttabs = "".join("-" for x in range(75))
+	linebreak = "\n" # f stringin içindeki curly bracketlarda backslah konulamıyormuş
+	
+	val = (False, False, "speaker")
+	mycursor.execute("SELECT name, team, institution FROM Participants WHERE checkin = %s AND cut_status = %s AND role = %s;", val)
+	myresult = mycursor.fetchall()
 
+	teamtable = [[f'{x[0][:25]:<30}{x[1][:20]:<25}{x[2][:20]:<20}' for x in myresult[y*20:(y*20+20)]] for y in range(int(math.ceil(len(myresult)/20)))]
+	team_title = f'{"İsim":<30}{"Takım":<25}{"Okul":20}\n'
+	
+	title_1 = f'✂️ Düşürülmek üzere olan takımlar ✂️'
+	title_2 = f'✂️ Düşürülmek üzere olan juriler ✂️'
 
+	val = (False, False, "jury")
+	mycursor.execute("SELECT name, institution FROM Participants WHERE checkin = %s AND cut_status = %s AND role = %s;", val)
+	myresult = mycursor.fetchall()
+
+	jurytable = [[f'{x[0][:25]:<30}{x[1][:20]:<20}' for x in myresult[y*20:(y*20+20)]] for y in range(int(math.ceil(len(myresult)/20)))]
+	jury_title = f'{"İsim":<30}{"Okul":20}\n'
+
+	await guild.get_channel(announcementId).delete_messages(cutMessageList)
+	
+	cutMessageList.clear()
+
+	if len(teamtable[0]) >= 1:
+		message = await guild.get_channel(announcementId).send(title_1)
+		cutMessageList.append(message)
+
+		firstCodeBlock = True
+
+		for x in teamtable:
+			if firstCodeBlock:
+				message = await guild.get_channel(announcementId).send(f'```{team_title}{alttabs}\n{linebreak.join(x)}```')
+				cutMessageList.append(message)
+				firstCodeBlock = False
+			else:
+				message = await guild.get_channel(announcementId).send(f'```{linebreak.join(x)}```')
+				cutMessageList.append(message)
+
+	if len(jurytable[0]) >= 1:	
+		message = await guild.get_channel(announcementId).send(title_2)
+		cutMessageList.append(message)
+
+		firstCodeBlock = True
+
+		for x in jurytable:
+			if firstCodeBlock:
+				message = await guild.get_channel(announcementId).send(f'```{jury_title}{alttabs[:50]}\n{linebreak.join(x)}```')
+				cutMessageList.append(message)
+				firstCodeBlock = False
+			else:
+				message = await guild.get_channel(announcementId).send(f'```{linebreak.join(x)}```')
+				cutMessageList.append(message)
+
+	
+
+client.run(TOKEN)
 
